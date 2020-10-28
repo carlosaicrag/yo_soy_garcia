d37142dfd8fac7e7719b0cc300aa8badabf74294
Carlos Garcia <carlosaicrag@gmail.com>
2020-10-27 23:11:58 -0700

    add dynamic centering for tree when rendered

--- app/javascript/frontend/components/family_tree_show/family_tree_show_component.js
+++ app/javascript/frontend/components/family_tree_show/family_tree_show_component.js
@@ -4,40 +4,41 @@ import Tree from 'react-d3-tree';
 export default class FamilyTreeShowComponent extends React.Component {
   constructor(props){
     super(props)
-    this.state = {fetching:true, translate: {x: 250,
-          y: 200}, centering: true}
+    this.state = {fetching:true, translate: {}, centering: true}
   }
 
   componentDidMount(){
     this.props.fetchFamilyTree(this.props.match.params.familyMemberId).then(() => {
-      this.setState({fetching:false})
+      let dimensions = this.treeContainer.getBoundingClientRect();
+      this.setState({
+        translate: {
+          x: dimensions.width / 2,
+          y: dimensions.height / 2 - 100
+        },
+        fetching: false
+      });
     })
   }
 
   createTree() {
-    dimensions = this.treeContainer.getBoundingClientRect();
-    this.setState({
-        translate: {
-          x: 250,
-          y: 250
-        },
-        centering: false
-      });
-  }
-
-  render(){
-    if (this.state.fetching){
-      return <div ref={tc => (this.treeContainer = "banana")}></div>
-    }
-    
-    return(
-      <div className="profile-container">
-        <div className="tree-wrapper" ref={tc => (this.treeContainer = tc)}>
-          <Tree className="treeWrapper" 
+    if(this.state.fetching){
+      return <div></div>
+    }else{
+      return <Tree className="treeWrapper" 
           data={this.props.familyMember.family_tree} 
           orientation="vertical"
           translate={this.state.translate}
           ></Tree>
+    }
+    
+  }
+
+  render(){
+    
+    return(
+      <div className="profile-container">
+        <div className="tree-wrapper" ref={tc => (this.treeContainer = tc)}>
+          {this.createTree()}
         </div>
       </div>
     )
