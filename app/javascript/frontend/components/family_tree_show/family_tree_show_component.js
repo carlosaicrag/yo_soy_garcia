8a2928c869dfc22dc967528d5ec764d26ac7e4f1
Carlos Garcia <carlosaicrag@gmail.com>
2020-10-27 17:35:05 -0700

    center tree within tree wrapper

--- app/javascript/frontend/components/family_tree_show/family_tree_show_component.js
+++ app/javascript/frontend/components/family_tree_show/family_tree_show_component.js
@@ -4,7 +4,8 @@ import Tree from 'react-d3-tree';
 export default class FamilyTreeShowComponent extends React.Component {
   constructor(props){
     super(props)
-    this.state = {fetching:true}
+    this.state = {fetching:true, translate: {x: 250,
+          y: 200}, centering: true}
   }
 
   componentDidMount(){
@@ -13,14 +14,31 @@ export default class FamilyTreeShowComponent extends React.Component {
     })
   }
 
+  createTree() {
+    dimensions = this.treeContainer.getBoundingClientRect();
+    this.setState({
+        translate: {
+          x: 250,
+          y: 250
+        },
+        centering: false
+      });
+  }
+
   render(){
     if (this.state.fetching){
-      return <div></div>
+      return <div ref={tc => (this.treeContainer = "banana")}></div>
     }
-
+    
     return(
-      <div id="profile-container">
-        <Tree className="treeWrapper" data={this.props.familyMember.family_tree} orientation="vertical"></Tree>
+      <div className="profile-container">
+        <div className="tree-wrapper" ref={tc => (this.treeContainer = tc)}>
+          <Tree className="treeWrapper" 
+          data={this.props.familyMember.family_tree} 
+          orientation="vertical"
+          translate={this.state.translate}
+          ></Tree>
+        </div>
       </div>
     )
   }
